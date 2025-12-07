bd24007de3dbc4d02e1b7719ff1dca74eaf0180d
kohinurfatema <kohinurfatema35@gmail.com>
2025-12-07 06:50:03 -0800

    making login and register page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,46 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router';
+
+
 
 const Login = () => {
+    const { register,handleSubmit, formState: { errors } } = useForm();
+
+     const handleLogin = (data) => {
+        console.log('form data', data);
+     }
+   
     return (
-        <div>
-            Login pages
+        <div className="card bg-base-100 w-full mx-auto max-w-sm shrink-0 shadow-2xl">
+            <h3 className="text-3xl text-center">Welcome back</h3>
+            <p className='text-center'>Please Login</p>
+            <form className="card-body" onSubmit={handleSubmit(handleLogin)}>
+                <fieldset className="fieldset">
+                    {/* email field */}
+                    <label className="label">Email</label>
+                    <input type="email" {...register('email', { required: true })} className="input" placeholder="Email" />
+                    {
+                        errors.email?.type === 'required' && <p className='text-red-500'>Email is required</p>
+                    }
+
+                    {/* password field */}
+                    <label className="label">Password</label>
+                    <input type="password" {...register('password', { required: true, minLength: 6 })} className="input" placeholder="Password" />
+                    {
+                        errors.password?.type === 'minLength' && <p className='text-red-500'>Password must be 6 characters  or longer </p>
+                    }
+
+
+                    <div><a className="link link-hover">Forgot password?</a></div>
+                    <button className="btn btn-neutral mt-4">Login</button>
+                </fieldset>
+                <p>New to Pothole Patrol <Link
+                    state={location.state}
+                    className='text-blue-400 underline'
+                    to="/register">Register</Link></p>
+            </form>
+           
         </div>
     );
 };
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,44 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router';
 
-const Register = () => {
+
+
+const Login = () => {
+    const { register, formState: { errors } } = useForm();
+   
     return (
-        <div>
-            register page 
+        <div className="card bg-base-100 w-full mx-auto max-w-sm shrink-0 shadow-2xl">
+            <h3 className="text-3xl text-center">Welcome back</h3>
+            <p className='text-center'>Please Register</p>
+            <form className="card-body">
+                <fieldset className="fieldset">
+                    {/* email field */}
+                    <label className="label">Email</label>
+                    <input type="email" {...register('email', { required: true })} className="input" placeholder="Email" />
+                    {
+                        errors.email?.type === 'required' && <p className='text-red-500'>Email is required</p>
+                    }
+
+                    {/* password field */}
+                    <label className="label">Password</label>
+                    <input type="password" {...register('password', { required: true, minLength: 6 })} className="input" placeholder="Password" />
+                    {
+                        errors.password?.type === 'minLength' && <p className='text-red-500'>Password must be 6 characters  or longer </p>
+                    }
+
+
+                    <div><a className="link link-hover">Forgot password?</a></div>
+                    <button className="btn btn-neutral mt-4">Register</button>
+                </fieldset>
+                <p>New to Pothole Patrol <Link
+                    state={location.state}
+                    className='text-blue-400 underline'
+                    to="/login">Login</Link></p>
+            </form>
+           
         </div>
     );
 };
 
-export default Register;
+export default Login;
